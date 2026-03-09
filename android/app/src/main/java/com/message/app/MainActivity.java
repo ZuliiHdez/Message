c0bf5a3ef7e7c3ac8b43863e0245f315cd5d7430
DESKTOP-GCHOR6D\zulei <zuleima2304@gmail.com>
2026-03-09 15:30:40 +0000

    feat: initial project setup Angular + Ionic + Capacitor

---
+++ android/app/src/main/java/com/message/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.message.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.message.app',
+  appName: 'message',
+  webDir: 'www'
+};
+
+export default config;
