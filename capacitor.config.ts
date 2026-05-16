4a517f1214449ecf14dac3d95a1498c075905726
Aythami <aypeve@gmail.com>
2026-05-16 21:52:57 +0100

    Correccion de Icono y nombre de App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.message.app',
-  appName: 'message',
+  appName: 'Orion',
   webDir: 'www'
 };
 
